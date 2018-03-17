b40d8cb00e2d54d4b014a883169185386e808085
Rafael Alves <rafaeldobau@gmail.com>
2018-03-17 01:14:29 -0300

    Improve performance using Map instead List

--- Main/src/main/java/Client.java
+++ Main/src/main/java/Client.java
@@ -1,22 +1,30 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Client {
 
 
     public int solution(int X, int[] A) {
-        List<Integer> list = new ArrayList<Integer>();
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
         for (int i = 0; i < A.length; i++) {
-            if (!list.contains(A[i]))
-                list.add(A[i]);
-        }
-        Integer element = list.get(list.size() - 1);
-        if (list.size() == X) {
-            for (int i = 0; i < A.length; i++) {
-                if (element == A[i]) {
-                    return i;
-                }
+            if (!map.containsKey(A[i])) {
+                map.put(A[i], i);
             }
         }
+
+        for (int i = 1; i < X; i++) {
+            if (!map.containsKey(i)) {
+                return -1;
+            }
+        }
+
+        Integer element = map.get(X);
+        if (element != null) {
+            return element;
+        }
+
         return -1;
     }
 
